7f9c153b6b4eae22c4fc229c7b168ab9deab787d
hansonwang99 <hansonwang99@163.com>
2017-06-02 21:33:00 +0800

    xxxx

--- src/main/java/com/hansonwang99/HelloWoldSpringController.java
+++ src/main/java/com/hansonwang99/HelloWoldSpringController.java
@@ -18,7 +18,7 @@ public class HelloWoldSpringController {
     //表示返回JSON格式的结果，如果前面使用的是@RestController可以不用写
     @ResponseBody
     String home() {
-        return "Hello World! woridddddfff";//返回结果为字符串
+        return "Hello World!";//返回结果为字符串
     }
 
     public static void main(String[] args) throws Exception {

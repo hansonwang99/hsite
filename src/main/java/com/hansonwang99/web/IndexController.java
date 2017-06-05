32add5b71a79ad4eb25b2b1e409331c62973bc86
hansonwang99 <hansonwang99@163.com>
2017-06-05 15:27:06 +0800

    add thymeleaf dependencies

--- src/main/java/com/hansonwang99/web/IndexController.java
+++ src/main/java/com/hansonwang99/web/IndexController.java
@@ -9,11 +9,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Created by hansonwang on 2017/6/2.
  */
 @Controller
-@RequestMapping("/")
 public class IndexController {
 
     @RequestMapping(value="/index",method= RequestMethod.GET)
     public String index(Model model) {
-        return "index";
+        return "login";
     }
 }

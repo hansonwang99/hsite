965c2fd8cd1458525db192bff47be0865126ee4d
hansonwang99 <hansonwang99@163.com>
2017-06-21 15:48:01 +0800

    now we can register !

--- src/main/java/com/hansonwang99/web/UserController.java
+++ src/main/java/com/hansonwang99/web/UserController.java
@@ -1,21 +1,55 @@
 package com.hansonwang99.web;
 
+import com.hansonwang99.comm.Const;
+import com.hansonwang99.domain.User;
+import com.hansonwang99.domain.result.ExceptionMsg;
+import com.hansonwang99.domain.result.Response;
+import com.hansonwang99.repository.UserRepository;
+import com.hansonwang99.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by hansonwang on 2017/6/18.
  */
 @RestController
 @RequestMapping("/user")
-public class UserController {
+public class UserController extends BaseController {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public void login(  ) {
 
         System.out.println("enter login");
     }
+
+    @RequestMapping(value = "/regist", method = RequestMethod.POST)
+    public Response create(User user) {
+        try {
+            User registUser = userRepository.findByEmail(user.getEmail());
+            if (null != registUser) {
+                return result(ExceptionMsg.EmailUsed);
+            }
+            User userNameUser = userRepository.findByUserName(user.getUserName());
+            if (null != userNameUser) {
+                return result(ExceptionMsg.UserNameUsed);
+            }
+            user.setPassWord(getPwd(user.getPassWord()));
+            user.setCreateTime(DateUtils.getCurrentTime());
+            user.setLastModifyTime(DateUtils.getCurrentTime());
+            user.setProfilePicture("img/favicon.png");
+            userRepository.save(user);
+
+        } catch (Exception e) {
+
+            logger.error("create user failed, ", e);
+            return result(ExceptionMsg.FAILED);
+        }
+        return result();
+    }
+
 }

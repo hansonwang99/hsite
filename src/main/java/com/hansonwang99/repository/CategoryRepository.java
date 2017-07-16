6f064128b9592c00eba13bd494c482bfbf4966db
hansonwang99 <hansonwang99@163.com>
2017-07-16 12:25:36 +0800

    now the category can show on the left bar

--- src/main/java/com/hansonwang99/repository/CategoryRepository.java
+++ src/main/java/com/hansonwang99/repository/CategoryRepository.java
@@ -3,10 +3,14 @@ package com.hansonwang99.repository;
 import com.hansonwang99.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/7/14.
  */
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Category findByUserIdAndName( Long userId, String name );
+
+    List<Category> findByUserIdOrderByIdDesc(Long userId);
 }
--- src/main/java/com/hansonwang99/web/CategoryController.java
+++ src/main/java/com/hansonwang99/web/CategoryController.java
@@ -7,11 +7,13 @@ import com.hansonwang99.repository.CategoryRepository;
 import com.hansonwang99.service.CategoryService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/category")
@@ -43,6 +45,27 @@ public class CategoryController extends BaseController{
 			return result(ExceptionMsg.FavoritesNameIsNull);
 		}
 		return result();
+	}
+
+	/**
+	 * 获取文章分类
+	 * @return
+	 */
+	@RequestMapping(value = "/getCategory/{userId}", method = RequestMethod.POST)
+	public List<Category> getFavorites(@PathVariable("userId") Long userId) {
+		List<Category> categories = null;
+		try {
+			Long id = getUserId();
+			if(null != userId && 0 != userId){
+				id = userId;
+			}
+			categories = categoryRepository.findByUserIdOrderByIdDesc(id);
+		} catch (Exception e) {
+			// TODO: handle exception
+			logger.error("getCategory failed, ", e);
+		}
+		logger.info("getCategory end ==" );
+		return categories;
 	}
 
 }

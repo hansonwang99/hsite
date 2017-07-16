d743d1828b4cfbee590b67a90c88f053b1b49078
hansonwang99 <hansonwang99@163.com>
2017-07-16 16:11:24 +0800

    xxxx

--- src/main/java/com/hansonwang99/repository/CategoryRepository.java
+++ src/main/java/com/hansonwang99/repository/CategoryRepository.java
@@ -2,7 +2,11 @@ package com.hansonwang99.repository;
 
 import com.hansonwang99.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 /**
@@ -13,4 +17,9 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByUserIdAndName( Long userId, String name );
 
     List<Category> findByUserIdOrderByIdDesc(Long userId);
+
+    @Modifying(clearAutomatically=true)
+    @Transactional
+    @Query("update Category c set c.count=(c.count+1),c.lastModifyTime =:lastModifyTime where c.id =:id")
+    void increaseCountById(@Param("id") Long id, @Param("lastModifyTime") Long lastModifyTime);
 }
--- src/main/java/com/hansonwang99/web/ArticleController.java
+++ src/main/java/com/hansonwang99/web/ArticleController.java
@@ -5,6 +5,7 @@ import com.hansonwang99.domain.result.ExceptionMsg;
 import com.hansonwang99.domain.result.Response;
 import com.hansonwang99.domain.result.ResponseData;
 import com.hansonwang99.repository.ArticleRepository;
+import com.hansonwang99.repository.CategoryRepository;
 import com.hansonwang99.service.ArticleService;
 import com.hansonwang99.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +23,19 @@ public class ArticleController extends BaseController {
     private ArticleRepository articleRepository;
 
     @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
     private ArticleService articleService;
 
     @RequestMapping(value = "/savaarticle", method = RequestMethod.POST)
     public ResponseData create(Article article) {
 
         try {
+
+            // 每增加一篇文章就需要增加其对应分类下的文章的计数
+            categoryRepository.increaseCountById( article.getCategoryId(), DateUtils.getCurrentTime() );
+
             article.setPublish_at( DateUtils.getCurrentTime() ); // 当前时间
             article.setUserId( getUserId() );                    // userId目前从内存中获取
             articleRepository.save( article );

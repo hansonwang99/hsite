f81054cded8f4d9c4dc9e0f8c9ebed7a3aa9fe9c
hansonwang99 <hansonwang99@163.com>
2017-08-23 15:59:33 +0800

    add swagger support

---
+++ src/main/java/com/hansonwang99/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.hansonwang99;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by Administrator on 2017/8/23.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket testApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("test")
+                .genericModelSubstitutes(DeferredResult.class)
+                .useDefaultResponseMessages(false)
+                .forCodeGeneration(true)
+                .pathMapping("/")// base，最终调用接口后会和paths拼接在一起
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.hansonwang99"))
+                .build();
+    }
+}

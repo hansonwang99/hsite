6fa241dd1a636519c6823745a840580f630cde8a
hansonwang99 <hansonwang99@163.com>
2017-08-24 10:34:57 +0800

    modify indexcontroller

--- src/main/java/com/hansonwang99/web/IndexController.java
+++ src/main/java/com/hansonwang99/web/IndexController.java
@@ -4,6 +4,7 @@ import com.hansonwang99.comm.Const;
 import com.hansonwang99.domain.Category;
 import com.hansonwang99.repository.CategoryRepository;
 import com.hansonwang99.service.CategoryService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,21 +27,19 @@ public class IndexController extends BaseController {
     @Resource
     private CategoryService categoryService;
 
+    @ApiOperation(value="显示网站首页视图", notes="显示网站首页视图")
     @RequestMapping(value="/",method= RequestMethod.GET)
     public String index(Model model) {
         return "index";
     }
 
+    @ApiOperation(value="显示用户登录视图", notes="显示用户登录视图")
     @RequestMapping(value="/login",method= RequestMethod.GET)
     public String login(Model model) {
         return "login";
     }
 
-    @RequestMapping(value="/layout",method= RequestMethod.GET)
-    public String layout(Model model) {
-        return "layout";
-    }
-
+    @ApiOperation(value="显示主框架视图", notes="显示主框架视图")
     @RequestMapping(value="/home",method= RequestMethod.GET)
     public String home(Model model) {
 
@@ -60,16 +59,19 @@ public class IndexController extends BaseController {
         return "home";
     }
 
+    @ApiOperation(value="显示弹出式编辑器md书写区视图", notes="显示弹出式编辑器md书写区视图")
     @RequestMapping(value="/write",method= RequestMethod.GET)
     public String write(Model model) {
         return "write";
     }
 
+    @ApiOperation(value="显示用户注册视图", notes="显示用户注册视图")
     @RequestMapping(value="/register",method= RequestMethod.GET)
     public String register(Model model) {
         return "register";
     }
 
+    @ApiOperation(value="显示用户登出返回网站首页视图", notes="显示用户登出返回网站首页视图")
     @RequestMapping(value="/logout",method=RequestMethod.GET)
     public String logout(HttpServletResponse response, Model model) {
         getSession().removeAttribute(Const.LOGIN_SESSION_KEY);
@@ -82,16 +84,19 @@ public class IndexController extends BaseController {
         return "index";
     }
 
+    @ApiOperation(value="显示上传用户图像视图", notes="显示上传用户图像视图")
     @RequestMapping(value="/uploadHeadPortrait",method = RequestMethod.POST)
     public String uploadHeadPortrait(){
         return "user/uploadheadportrait";
     }
 
+    @ApiOperation(value="显示新建文章分类视图", notes="显示新建文章分类视图")
     @RequestMapping(value="/newCategory",method = RequestMethod.POST)
     public String newCategory(){
         return "category/newcategory";
     }
 
+    @ApiOperation(value="显示身高百分位表视图", notes="显示身高百分位表视图")
     @RequestMapping(value="/heightsheet",method = RequestMethod.POST)
     public String getHeightSheet(Model model){
         return "tool/heightsheet";

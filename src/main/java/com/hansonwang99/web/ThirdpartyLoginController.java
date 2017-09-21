da9b095481f1e62acca74e6b02f529c5f05257dc
hansonwang99 <hansonwang99@163.com>
2017-09-21 17:10:07 +0800

    xxxx

--- src/main/java/com/hansonwang99/web/ThirdpartyLoginController.java
+++ src/main/java/com/hansonwang99/web/ThirdpartyLoginController.java
@@ -59,47 +59,62 @@ public class ThirdpartyLoginController extends BaseController {
 
         count++;
 
-        String client_id = "f15278f5c4d3438a1696";
-        String client_secret = "07f6fcfddf1897c18e1ceb819b08751e994ba0ed";
-        String redirect_uri = "http://localhost/thirdpartlogin/github";
+        String client_id = "5f4e0e7158ef656b3575";
+        String client_secret = "5160e0bbd4b2236a5fd228660d5ecf711fb60af5";
         String ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token?client_id=" +client_id+ "&client_secret=" +client_secret+ "&code=" +code;
+
 
         if( count<2 ) {
 
             String res = HttpRequest.sendPost(ACCESS_TOKEN_URL,null);
             access_token = res.split("&")[0];
 
+            System.out.println("111");
+
             String USERINFO_URL = "https://api.github.com/user?"+access_token;
             finaRes = HttpRequest.sendGet(USERINFO_URL);
             System.out.println(finaRes);
+            System.out.println("222");
         } else
             count = 0;
 
         JSONObject githubUserInfo = (JSONObject) JSON.parse(finaRes);
 
+        System.out.println("333");
+
         String userName  = githubUserInfo.getString("login");
         String userEmail = githubUserInfo.getString("email");
         User loginUser = userRepository.findByUserName(userName);
+
+        System.out.println("444");
+
         if (loginUser == null ) { // 用户不存在，则应该将用户存到数据库中
             loginUser = new User();
             loginUser.setUserName( userName );
             if( null==userEmail )
-                userEmail = "null@null.com";
+                userEmail = "null@" + userName + "null.com";
             loginUser.setEmail( userEmail );
             loginUser.setPassWord( "nullpassword" );  // 通过第三方登录的用户是没有密码的，给个默认值
             loginUser.setCreateTime(DateUtils.getCurrentTime());
             loginUser.setLastModifyTime(DateUtils.getCurrentTime());
             loginUser.setProfilePicture( githubUserInfo.getString("avatar_url") );
             userRepository.save(loginUser);
+
+            System.out.println("555");
         }
 
         loginUser = userRepository.findByUserName(userName);
+
+
+        System.out.println("666");
 
         Cookie cookie = new Cookie(Const.LOGIN_SESSION_KEY, cookieSign(loginUser.getId().toString()));
         cookie.setMaxAge(Const.COOKIE_TIMEOUT);
         cookie.setPath("/");
         response.addCookie(cookie);
         getSession().setAttribute(Const.LOGIN_SESSION_KEY, loginUser);
+
+        System.out.println("777");
 
         String defaultCategoryName = "默认分类";
         Category category = categoryRepository.findByUserIdAndName( getUserId(), defaultCategoryName );
@@ -112,6 +127,8 @@ public class ThirdpartyLoginController extends BaseController {
                 logger.error("创建默认分类出现异常：",e);
             }
         }
+
+        System.out.println("888");
 
         model.addAttribute("user",getUser());
 
